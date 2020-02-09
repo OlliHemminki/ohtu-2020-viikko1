cabd71d2f7675279dce01bfc167213594bcd1016
OlliHemminki <olli.hemminki@gmail.com>
2020-02-09 15:04:07 +0200

    Vieläkin codeclimate-testailua

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
     	
-    	if(true)
-    	{
-    		System.out.println("Tämän pitäisi rikkoa tyylisääntöä");
-    	}
+//    	if(true)
+//    	{
+//    		System.out.println("Tämän pitäisi rikkoa tyylisääntöä");
+//    	}
     	
         System.out.println("Pääohjelman koodi poistettu tehtävänannon neuvon mukaisesti, jotta koodista saadaan tyylisääntöjen mukainen.");
     }

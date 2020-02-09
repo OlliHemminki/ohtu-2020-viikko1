df1807036523f598e76ec00c61f0d79829f4eb17
OlliHemminki <olli.hemminki@gmail.com>
2020-02-09 14:48:36 +0200

    Rikottu tyyliehtoja codeclimate-testin vuoksi

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,12 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	if(true)
+    	{
+    		System.out.println("Tämän pitäisi rikkoa tyylisääntöä");
+    	}
+    	
         System.out.println("Pääohjelman koodi poistettu tehtävänannon neuvon mukaisesti, jotta koodista saadaan tyylisääntöjen mukainen.");
     }
 }
